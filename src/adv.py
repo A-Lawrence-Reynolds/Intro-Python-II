7f55b78272cc8260e98c145d343ff22cd69f79eb
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-03-12 08:10:02 -0700

    fixed some small bugs

--- src/adv.py
+++ src/adv.py
@@ -1,103 +1,3 @@
-# from room import Room
-# from player import Player
-# # Declare all the rooms
-
-# room = {
-#     'outside':  Room("Outside Cave Entrance",
-#                      "North of you, the cave mount beckons"),
-
-#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-# passages run north and east."""),
-
-#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-# into the darkness. Ahead to the north, a light flickers in
-# the distance, but there is no way across the chasm."""),
-
-#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-# to north. The smell of gold permeates the air."""),
-
-#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south."""),
-# }
-
-
-# # Link rooms together
-
-# room['outside'].n_to = room['foyer']
-# room['foyer'].s_to = room['outside']
-# room['foyer'].n_to = room['overlook']
-# room['foyer'].e_to = room['narrow']
-# room['overlook'].s_to = room['foyer']
-# room['narrow'].w_to = room['foyer']
-# room['narrow'].n_to = room['treasure']
-# room['treasure'].s_to = room['narrow']
-
-# #
-# # Main
-# #
-
-# # Make a new player object that is currently in the 'outside' room.
-# Player_one = Player('Vitruvius', 'outside')
-# # Write a loop that:
-# #
-# while True:
-#     current_room = Player_one.current_room
-#     print(room[current_room])
-#     user_input = input(
-#         "Select a move(Moves to take are N,S,E,W or press Q to Quit)")
-# # OUTSIDE path
-# if Player_one.current_room == 'outside':
-#         if user_input == "N":
-#             Player_one.current_room = 'foyer'
-#         elif user_input == "q":
-#             break
-#         elif user_input != "N":
-#             print("Foward only")
-#     elif Player_one.current_room == 'foyer':
-#         if user_input == "S":
-#             Player_one.current_room = 'outside'
-#         elif user_input == "N":
-#             Player_one.current_room = 'overlook'
-#         elif user_input == "E":
-#             Player_one.current_room = 'narrow'
-#         elif user_input == "q":
-#             break
-#         elif user_input != "N" or "S" or "E":
-#             print("Not a path")
-#     elif Player_one.current_room == 'overlook':
-#         if user_input == "S":
-#             Player_one.current_room = 'foyer'
-#         elif user_input == "q":
-#             break
-#         elif user_input != "S":
-#             print("Not a path")
-#     elif Player_one.current_room == 'narrow':
-#         if user_input == "W":
-#             Player_one.current_room = 'foyer'
-#         elif user_input == "N":
-#             Player_one.current_room = 'treasure'
-#         elif user_input == "q":
-#             break
-#         elif user_input != "W" or "N":
-#             print("Not a path")
-#     elif Player_one.current_room == 'treasure':
-#         if user_input == "S":
-#             Player_one.current_room = 'narrow'
-#         elif user_input == "q":
-#             break
-#         elif user_input != "S":
-#             print("Not a path")
-
-
-# # * Prints the current room name
-# # * Prints the current description (the textwrap module might be useful here).
-# # * Waits for user input and decides what to do.
-# #
-# # If the user enters a cardinal direction, attempt to move to the room there.
-# # Print an error message if the movement isn't allowed.
-# #
-# # If the user enters "q", quit the game.
 from room import Room
 from player import Player
 import random
@@ -117,10 +17,10 @@ passages run north and east.""",f'loot: {pick2}'),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""",f'{pick3}'),
+the distance, but there is no way across the chasm.""",f'loot: {pick3}'),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""",f'{pick4}'),
+to north. The smell of gold permeates the air.""",f'loot: {pick4}'),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
@@ -145,7 +45,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-Player_one = Player("Joshua",'outside')
+Player_one = Player("Vitruvius",'outside')
 # print(Player_one.current_room)
 # Write a loop that:
 #
